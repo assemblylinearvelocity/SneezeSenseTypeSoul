fce12787f1c230a67677ca97e12a676661a60452
ftob <mpetrescu523@gmail.com>
2026-05-03 18:07:28 +0300

    move hp text above bar top left aligned

--- Game/Visuals/EspRenderer.lua
+++ Game/Visuals/EspRenderer.lua
@@ -190,7 +190,7 @@ function EspRenderer:UpdateHealthBar(min, max, character, showText)
 
     if showText then
         self.healthText.Text     = math.floor(hp) .. "/" .. math.floor(maxHp)
-        self.healthText.Position = Vector2.new(barX - 3, top + (height / 2) - 6)
+        self.healthText.Position = Vector2.new(barX - 1, top - 13)
         self.healthText.Color    = Color3.fromRGB(255, 255, 255)
         self.healthText.Visible  = true
     else
